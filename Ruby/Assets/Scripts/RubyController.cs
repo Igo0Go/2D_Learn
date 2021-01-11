536389e4320e40ec0763e5fd311f2955829133d5
Igo_Go <chudakin98@gmail.com>
2021-01-11 17:17:30 +0700

    Базовое перемещение персонажа

---
+++ Ruby/Assets/Scripts/RubyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1,10)]
+    private float movementSpeed = 3;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x += movementSpeed * horizontal * Time.deltaTime;
+        position.y += movementSpeed * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
